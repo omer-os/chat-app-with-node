3b43d46923d0e4875e3742bb53baf5078a2d27e3
omar chatin <omerchatin19@gmail.com>
2022-07-11 23:59:46 +0300

    big changes

--- app.js
+++ app.js
@@ -4,52 +4,114 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-// const mongoose = require("mongoose");
-// const todoSchema = new mongoose.Schema({
-//   todo: String,
-// });
-// async function main() {
-//   mongoose
-//     .connect(
-//       "mongodb+srv://user-1:t9ZnSpFMqNG2j34u@cluster0.hsbvm.mongodb.net/?retryWrites=true&w=majority",
-//       { useNewUrlParser: true, useUnifiedTopology: true }
-//     )
-//     .then(() => {
-//       console.log("Connected to database!");
-//     });
-// }
-// main()
-//   .catch(() => console.log("Error connecting to database"))
-//   .then(() => {
-//     app.listen(3000);
-//   });
-// const Todo = mongoose.model("Todo", todoSchema);
-
-// style in ./public/style.css
-// script in ./public/script.js
 app.use(express.static("public"));
 app.listen(process.env.PORT || 5000);
 
-const get_todos = (req, res) => {
-  Todo.find({}, (err, todos) => {
-    res.render("todo", { todos: todos });
-  });
-};
-
-const post_todos = (req, res) => {
-  const item = req.body.todo;
-  const newTodo = new Todo({
-    todo: item,
-  });
-  newTodo.save();
-  res.redirect("/todo");
-};
-
-app.get("/todo", (req, res) => {
-  res.render("todo");
-});
-app.post("/todo", post_todos);
+const chats = [
+  {
+    name: "Lily Ramirez",
+    message: "Ju ia anks",
+    time: "11:02",
+    img: "https://images.unsplash.com/photo-1657299170950-87e5b0eaf77c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    id: "cab54ac5-67c1-5bb7-85c6-ce8e78c5d5cc",
+    messeges: [
+      {
+        name: "person",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+      {
+        name: "me",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+    ],
+    messeges: [
+      {
+        name: "person",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+      {
+        name: "me",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+      {
+        name: "me",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+      {
+        name: "me",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+    ],
+  },
+  {
+    name: "Harold Daniels",
+    message: "Da ny orale orale orale orale orale orale lojhwor wen s",
+    time: "05:02",
+    img: "https://images.unsplash.com/photo-1657530007077-03b920b8bc12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    id: "4215d62c-adcf-5ec4-b2db-3eb1492f8d04",
+    messeges: [
+      {
+        name: "person",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+      {
+        name: "me",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+    ],
+  },
+  {
+    name: "Sarah Strickland",
+    message: "Jeff ry unnin gham",
+    time: "02:02",
+    img: "https://images.unsplash.com/photo-1657563495719-e3693698c9c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    id: "df7aa668-c020-5d15-96bb-1a47ff2d2ed8",
+    messeges: [
+      {
+        name: "person",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+      {
+        name: "me",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+    ],
+  },
+];
 
 app.get("/", (req, res) => {
-  res.render("todo");
+  res.render("home", {
+    chats,
+    chat: false,
+  });
+});
+
+app.get("/rooms/:id", (req, res) => {
+  const id = req.params.id;
+  const chat = chats.find((item) => item.id === id).messeges;
+  res.render("room", {
+    chat,
+    chats,
+    chat_id: id,
+  });
+});
+app.post("/rooms/:id", (req, res) => {
+  const id = req.params.id;
+  const chat = chats.find((item) => item.id === id);
+  chat.messeges.push({
+    name: req.body.name,
+    message: req.body.message,
+    time: req.body.time,
+  });
+  res.redirect(`/rooms/${id}`);
 });
