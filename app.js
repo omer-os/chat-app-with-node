c36f5cafd27ff4c198595af63846ec622d7bbcb4
omar chatin <omerchatin19@gmail.com>
2022-07-12 00:26:03 +0300

    big changes

--- app.js
+++ app.js
@@ -69,6 +69,20 @@ const chats = [
     ],
   },
   {
+    name: "test",
+    message: "this chat is for testing",
+    time: "02:02",
+    img: "https://images.unsplash.com/photo-1657563495719-e3693698c9c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    id: "test",
+    messeges: [
+      {
+        name: "person",
+        message: "Ju ia anks",
+        time: "11:02",
+      },
+    ],
+  },
+  {
     name: "Sarah Strickland",
     message: "Jeff ry unnin gham",
     time: "02:02",
@@ -86,13 +100,14 @@ const chats = [
         time: "11:02",
       },
     ],
-  },
+  }
 ];
 
 app.get("/", (req, res) => {
   res.render("home", {
     chats,
     chat: false,
+    chat_id:"test",
   });
 });
 
